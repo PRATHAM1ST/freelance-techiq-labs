74dfab694c0817247ec1ad81143c3652b734f29d
Pratham <chudasamapratham142@gmail.com>
2024-04-25 16:13:05 +0530

    Implement carousel, video, and gif functionality

--- script.js
+++ script.js
@@ -1,52 +1,93 @@
-const allContainers = document.querySelectorAll(".container");
-const allVideos = document.querySelectorAll("video");
-const allCarousel = document.querySelectorAll(".content-carousel");
+// Select all elements
+const containers = document.querySelectorAll(".container");
+const videos = document.querySelectorAll("video");
+const carousels = document.querySelectorAll(".content-carousel");
 
+// Store opened containers
 const openedContainers = {};
 
-let gsapScroll = false;
-
-let currentContainer = null;
-
-let containerCursorPositionClicked = {
+// Flags
+let isGsapScrolling = false;
+let activeContainer = null;
+let cursorPosition = {
 	left: false,
 	right: false,
 };
 
+// Initial body scale
 gsap.to(document.body, {
 	duration: 1,
 	scale: 0.75,
 });
 
-async function expandthecontainer(container) {
+// Function to handle mouse movement within container
+const handleMouseMovement = (element, mouseX) => {
+	if (mouseX > window.innerWidth * 0.8) {
+		element.style.cursor = "var(--cursor-right)";
+		cursorPosition.right = true;
+	} else if (mouseX < window.innerWidth * 0.2) {
+		element.style.cursor = "var(--cursor-left)";
+		cursorPosition.left = true;
+	} else {
+		cursorPosition.left = false;
+		cursorPosition.right = false;
+	}
+	return cursorPosition.left || cursorPosition.right;
+};
+
+// Function to check if video is playing
+const isVideoPlaying = (video) =>
+	!!(
+		video.currentTime > 0 &&
+		!video.paused &&
+		!video.ended &&
+		video.readyState > 2
+	);
+
+// Function to expand container
+async function expandContainer(container) {
+	// Register GSAP plugins
 	gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable);
 
-	Draggable.create(container, {
-		type: "x",
-		bounds: ".maincontainer",
-	});
+	// Make the container draggable horizontally within bounds
+	// Draggable.create(container, {
+	// 	type: "left",
+	// 	bounds: ".maincontainer",
+	// });
 
-	var viewHeight = window.innerHeight;
-	var targetHeight = viewHeight * 0.6;
-	var y = (viewHeight - 590) / 2;
+	// openedContainers[container.id].sections.forEach((section) => {
+	// 	Draggable.create(section, {
+	// 		type: "left",
+	// 		bounds: ".container",
+	// 	});
+	// });
 
-	gsapScroll = true;
+	// Calculate target height and vertical offset for snapping
+	const viewHeight = window.innerHeight;
+	const targetHeight = viewHeight * 0.6;
+	const verticalOffset = (viewHeight - 590) / 2;
 
+	// Set flag for GSAP scroll animation
+	isGsapScrolling = true;
+
+	// Scale up the body
 	await gsap.to(document.body, {
 		duration: 1,
 		scale: 1,
 	});
 
+	// Snap to the container
 	const snapping = gsap.to(window, {
 		duration: 1,
 		focus: true,
 		ease: "power2.inOut",
 		scrollTo: {
 			y: container,
-			offsetY: y,
+			offsetY: verticalOffset,
 		},
 	});
 
+	// Animate the container's expansion
 	const containerAnimation = gsap.to(container, {
 		duration: 1,
 		x: 0,
@@ -62,90 +103,76 @@ async function expandthecontainer(container) {
 		cursor: "grab",
 	});
 
-	const revealHiddenContentAnimation = gsap.to(
-		"#" + container.id + "> .hidden",
-		{
-			duration: 1,
-			overflow: "visible",
-			width: "fit-content",
-			ease: "power2.inOut",
-			stagger: 0.3,
-		}
-	);
+	// Reveal hidden content within the container
+	const revealHiddenContentAnimation = gsap.to(`#${container.id} > .hidden`, {
+		duration: 1,
+		overflow: "visible",
+		width: "fit-content",
+		ease: "power2.inOut",
+		stagger: 0.3,
+	});
 
+	// Wait for all animations to complete
 	await Promise.all([
 		snapping,
 		containerAnimation,
 		revealHiddenContentAnimation,
 	]);
 
-	gsapScroll = false;
+	// Reset GSAP scroll flag
+	isGsapScrolling = false;
 
-	currentContainer = container;
+	// Set the current container
+	activeContainer = container;
 
+	// Retrieve sections within the container
 	const sections = openedContainers[container.id].sections;
 
+	// Initialize the current section
 	let currentSection = openedContainers[container.id].currentSection;
 
+	// Handle mouse movement within the container
 	container.addEventListener("mousemove", (e) => {
-		const containerWidth = container.offsetWidth;
-		const containerLeft = container.getBoundingClientRect().left;
-		const mouseEnterX = e.clientX;
-
-		// Check if mouse entered from the right side of 10% of the container width
-		if (mouseEnterX > containerLeft + containerWidth * 0.8) {
-			container.style.cursor = "var(--cursor-right)";
-			containerCursorPositionClicked.right = true;
-		} else if (mouseEnterX < containerLeft + containerWidth * 0.2) {
-			container.style.cursor = "var(--cursor-left)";
-			containerCursorPositionClicked.left = true;
-		} else {
-			container.style.cursor = "grab";
-			containerCursorPositionClicked.left = false;
-			containerCursorPositionClicked.right = false;
-		}
+		const mouseX = e.clientX;
+		if (handleMouseMovement(container, mouseX)) return;
+		container.style.cursor = "grab";
 	});
 
+	// Handle click events within the container
 	container.addEventListener("click", (e) => {
-		if (containerCursorPositionClicked.right) {
+		if (cursorPosition.right) {
 			currentSection = (currentSection + 1) % sections.length;
-			gsap.to(container, {
-				duration: 1,
-				scrollTo: {
-					x: sections[currentSection].offsetLeft,
-					offsetX:
-						(window.innerWidth -
-							sections[currentSection % sections.length]
-								.offsetWidth) /
-						2,
-				},
-				cursor: "var(--cursor-right)",
-			});
-		} else if (containerCursorPositionClicked.left) {
+		} else if (cursorPosition.left) {
 			currentSection =
 				currentSection === 0 ? sections.length - 1 : currentSection - 1;
-			gsap.to(container, {
-				duration: 1,
-				scrollTo: {
-					x: sections[currentSection].offsetLeft,
-					offsetX:
-						(window.innerWidth -
-							sections[currentSection % sections.length]
-								.offsetWidth) /
-						2,
-				},
-				cursor: "var(--cursor-left)",
-			});
 		}
 
+		// Scroll to the selected section
+		gsap.to(container, {
+			duration: 1,
+			scrollTo: {
+				x: sections[currentSection].offsetLeft,
+				offsetX:
+					(window.innerWidth -
+						sections[currentSection % sections.length]
+							.offsetWidth) /
+					2,
+			},
+			cursor: cursorPosition.right
+				? "var(--cursor-right)"
+				: "var(--cursor-left)",
+		});
+
+		// Update the current section index
 		openedContainers[container.id].currentSection = currentSection;
 	});
 }
 
+// Event listener for scroll
 lenis.on("scroll", (e) => {
-	if (gsapScroll) return;
+	if (isGsapScrolling) return;
 
-	gsap.to(currentContainer, {
+	gsap.to(activeContainer, {
 		width: "133.33%",
 		x: "-12.5%",
 		overflowX: "hidden",
@@ -156,20 +183,22 @@ lenis.on("scroll", (e) => {
 		scale: Math.max(0.7, 0.75 - Math.abs(e.velocity / 100)),
 	});
 
-	currentContainer = null;
+	activeContainer = null;
 });
 
-allContainers.forEach((container) => {
+// Event listener for container clicks
+containers.forEach((container) => {
 	container.addEventListener("click", () => {
-		if (gsapScroll) return;
-		if (currentContainer) return;
+		// Check if GSAP scroll is in progress or a container is already expanded
+		if (isGsapScrolling || activeContainer) return;
 
-		// if id of the container is not in the openedContainers object
+		// Check if the container is not already in the openedContainers object
 		if (!openedContainers[container.id]) {
+			// Initialize the container sections
 			openedContainers[container.id] = {
 				currentSection: 0,
 				sections: [
-					container.childNodes[3],
+					container.childNodes[3], // Assuming this is the first section
 					...[...container.childNodes[5].childNodes].filter(
 						(node) => node.nodeName === "DIV"
 					),
@@ -177,76 +206,36 @@ allContainers.forEach((container) => {
 			};
 		}
 
-		// window.location.href = window.location.origin + "#" + container.id;
+		// Enable horizontal scrolling for the body
 		document.body.style.overflowX = "scroll";
-		expandthecontainer(container);
+
+		// Expand the clicked container
+		expandContainer(container);
 	});
 });
 
-allVideos.forEach((video) => {
+// Event listener for video mouse movements and clicks
+videos.forEach((video) => {
 	video.addEventListener("mousemove", (e) => {
-		const mouseEnterX = e.clientX;
-		if (mouseEnterX > window.innerWidth * 0.8) {
-			video.style.cursor = "var(--cursor-right)";
-			containerCursorPositionClicked.right = true;
-		} else if (mouseEnterX < window.innerWidth * 0.2) {
-			video.style.cursor = "var(--cursor-left)";
-			containerCursorPositionClicked.left = true;
-		} else {
-			containerCursorPositionClicked.left = false;
-			containerCursorPositionClicked.right = false;
-		}
+		const mouseX = e.clientX;
 
-		if (
-			containerCursorPositionClicked.left ||
-			containerCursorPositionClicked.right
-		)
-			return;
+		// Check if mouse movement occurs within a container, handle it and return
+		if (handleMouseMovement(video, mouseX)) return;
 
-		const isVideoPlaying = (video) =>
-			!!(
-				video.currentTime > 0 &&
-				!video.paused &&
-				!video.ended &&
-				video.readyState > 2
-			);
-		if (isVideoPlaying(video)) {
-			video.style.cursor = "var(--cursor-pause)";
-		} else {
-			video.style.cursor = "var(--cursor-play)";
-		}
+		// Check if the video is playing and update cursor accordingly
+		video.style.cursor = isVideoPlaying(video)
+			? "var(--cursor-pause)"
+			: "var(--cursor-play)";
 	});
 
-	// video.addEventListener("mouseout", () => {
-	// 	video.style.cursor = "var(--cursor-pointer)";
-	// });
-
+	// Event listener for video clicks
 	video.addEventListener("click", (e) => {
-		const mouseEnterX = e.clientX;
-		if (mouseEnterX > window.innerWidth * 0.8) {
-			video.style.cursor = "var(--cursor-right)";
-			containerCursorPositionClicked.right = true;
-		} else if (mouseEnterX < window.innerWidth * 0.2) {
-			video.style.cursor = "var(--cursor-left)";
-			containerCursorPositionClicked.left = true;
-		} else {
-			containerCursorPositionClicked.left = false;
-			containerCursorPositionClicked.right = false;
-		}
+		const mouseX = e.clientX;
 
-		if (
-			containerCursorPositionClicked.left ||
-			containerCursorPositionClicked.right
-		)
-			return;
+		// Check if mouse click occurs within a container, handle it and return
+		if (handleMouseMovement(video, mouseX)) return;
 
-		const isVideoPlaying = (video) =>
-			!!(
-				video.currentTime > 0 &&
-				!video.paused &&
-				!video.ended &&
-				video.readyState > 2
-			);
+		// Check if the video is playing, pause or play accordingly
 		if (isVideoPlaying(video)) {
 			video.pause();
 			video.style.cursor = "var(--cursor-play)";
@@ -257,9 +246,10 @@ allVideos.forEach((video) => {
 	});
 });
 
-allCarousel.forEach((carousel) => {
-	// loop trhorugh carousel items
-	const removeInterval = setInterval(() => {
+// Event listener for carousel mouse movements and clicks
+carousels.forEach((carousel) => {
+	// Set interval to auto-scroll the carousel
+	let autoScrollInterval = setInterval(() => {
 		const carouselWidth = carousel.offsetWidth;
 		const scrolling =
 			carousel.scrollLeft === carousel.scrollWidth - carouselWidth
@@ -273,85 +263,53 @@ allCarousel.forEach((carousel) => {
 		});
 	}, 3000);
 
+	// Event listener for mouse movements on the carousel
 	carousel.addEventListener("mousemove", (e) => {
 		const carouselWidth = carousel.offsetWidth;
 		const carouselLeft = carousel.getBoundingClientRect().left;
+		const mouseX = e.clientX;
 
-		const mouseEnterX = e.clientX;
-		if (mouseEnterX > window.innerWidth * 0.8) {
-			carousel.style.cursor = "var(--cursor-right)";
-			containerCursorPositionClicked.right = true;
-		} else if (mouseEnterX < window.innerWidth * 0.2) {
-			carousel.style.cursor = "var(--cursor-left)";
-			containerCursorPositionClicked.left = true;
-		} else {
-			containerCursorPositionClicked.left = false;
-			containerCursorPositionClicked.right = false;
-		}
+		// Check if mouse movement occurs within the carousel, handle it and return
+		if (handleMouseMovement(carousel, mouseX)) return;
 
-		if (
-			containerCursorPositionClicked.left ||
-			containerCursorPositionClicked.right
-		)
-			return;
-
-		if (mouseEnterX > carouselLeft + carouselWidth * 0.5) {
-			carousel.style.cursor = "var(--cursor-carousel-right)";
-		} else {
-			carousel.style.cursor = "var(--cursor-carousel-left)";
-		}
+		// Update cursor based on mouse position
+		carousel.style.cursor =
+			mouseX > carouselLeft + carouselWidth * 0.5
+				? "var(--cursor-carousel-right)"
+				: "var(--cursor-carousel-left)";
 	});
 
+	// Event listener for carousel clicks
 	carousel.addEventListener("click", async (e) => {
-		e.stopPropagation();
-		clearInterval(removeInterval);
+		clearInterval(autoScrollInterval);
 
 		const carouselWidth = carousel.offsetWidth;
 		const carouselLeft = carousel.getBoundingClientRect().left;
-		const mouseEnterX = e.clientX;
+		const mouseX = e.clientX;
 
-		if (mouseEnterX > window.innerWidth * 0.8) {
-			carousel.style.cursor = "var(--cursor-right)";
-			containerCursorPositionClicked.right = true;
-		} else if (mouseEnterX < window.innerWidth * 0.2) {
-			carousel.style.cursor = "var(--cursor-left)";
-			containerCursorPositionClicked.left = true;
-		} else {
-			containerCursorPositionClicked.left = false;
-			containerCursorPositionClicked.right = false;
-		}
+		// Check if click occurs within the carousel, handle it and return
+		if (handleMouseMovement(carousel, mouseX)) return;
 
-		if (
-			containerCursorPositionClicked.left ||
-			containerCursorPositionClicked.right
-		)
-			return;
-
-		if (mouseEnterX > carouselLeft + carouselWidth * 0.5) {
-			const scrolling =
-				carousel.scrollLeft === carousel.scrollWidth - carouselWidth
+		// Determine scrolling direction based on click position
+		const scrolling =
+			mouseX > carouselLeft + carouselWidth * 0.5
+				? carousel.scrollLeft === carousel.scrollWidth - carouselWidth
 					? 0
-					: carousel.scrollLeft + carouselWidth;
-			await gsap.to(carousel, {
-				duration: 1,
-				scrollTo: {
-					x: scrolling,
-				},
-			});
-		} else {
-			const scrolling =
-				carousel.scrollLeft === 0
-					? carousel.scrollWidth - carouselWidth
-					: carousel.scrollLeft + carouselWidth;
-			await gsap.to(carousel, {
-				duration: 1,
-				scrollTo: {
-					x: scrolling,
-				},
-			});
-		}
+					: carousel.scrollLeft + carouselWidth
+				: carousel.scrollLeft === 0
+				? carousel.scrollWidth - carouselWidth
+				: carousel.scrollLeft - carouselWidth;
 
-		removeInterval = setInterval(() => {
+		// Scroll to the calculated position
+		await gsap.to(carousel, {
+			duration: 1,
+			scrollTo: {
+				x: scrolling,
+			},
+		});
+
+		// Restart auto-scrolling interval
+		autoScrollInterval = setInterval(() => {
 			const carouselWidth = carousel.offsetWidth;
 			const scrolling =
 				carousel.scrollLeft === carousel.scrollWidth - carouselWidth
