dfbf4344998384e2fd8d399a55e66b7fe57cf241
Pratham <chudasamapratham142@gmail.com>
2024-04-26 00:31:47 +0530

    responsive

--- script.js
+++ script.js
@@ -26,14 +26,12 @@ gsap.to(document.body, {
 // Function to handle mouse movement within container
 const handleMouseMovement = (element, mouseX) => {
 	if (mouseX > window.innerWidth * 0.8) {
-		console.log("right");
 		activeContainer.style.cursor = "var(--cursor-right)";
 		element.style.cursor = "var(--cursor-right)";
 		cursorPosition.right = true;
 		cursorPosition.left = false;
 		return true;
 	} else if (mouseX < window.innerWidth * 0.2) {
-		console.log("left");
 		activeContainer.style.cursor = "var(--cursor-left)";
 		element.style.cursor = "var(--cursor-left)";
 		cursorPosition.left = true;
@@ -57,135 +55,241 @@ const isVideoPlaying = (video) =>
 
 // Function to expand container
 async function expandContainer(container) {
-	console.log(container);
 	// Register GSAP plugins
 	gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable);
 
 	// Make the container draggable horizontally within bounds
-	// Draggable.create(container, {
-	// 	type: "left",
-	// 	bounds: ".maincontainer",
-	// });
+	if (isMobile) {
+		// Draggable.create(container, {
+		// 	type: "x",
+		// 	bounds: ".maincontainer",
+		// });
 
-	// openedContainers[container.id].sections.forEach((section) => {
-	// 	Draggable.create(section, {
-	// 		type: "left",
-	// 		bounds: ".container",
-	// 	});
-	// });
+		const viewHeight = window.innerHeight;
+		const targetHeight =
+			openedContainers[container.id].targetHeight ??
+			container.childNodes[3].offsetHeight;
+		const bottomPadding =
+			openedContainers[container.id].bottomPadding ??
+			container.childNodes[1].offsetHeight;
+		const verticalOffset = (viewHeight - 590) / 2;
 
-	// Calculate target height and vertical offset for snapping
-	const viewHeight = window.innerHeight;
-	const targetHeight = viewHeight * 0.6;
-	const verticalOffset = (viewHeight - 590) / 2;
+		console.log("targetHeight", targetHeight);
 
-	// Set flag for GSAP scroll animation
-	isGsapScrolling = true;
-
-	// Scale up the body
-	await gsap.to(document.body, {
-		duration: 1,
-		scale: 1,
-	});
-
-	// Snap to the container
-	const snapping = gsap.to(window, {
-		duration: 1,
-		focus: true,
-		ease: "power2.inOut",
-		scrollTo: {
-			y: container,
-			offsetY: verticalOffset,
-		},
-	});
-
-	// Animate the container's expansion
-	const containerAnimation = gsap.to(container, {
-		duration: 1,
-		x: isMobile ? "-5%" : 0,
-		width: isMobile ? "111.11%" : "100%",
-		height: targetHeight,
-		overflowX: "scroll",
-		margin: "5rem 0",
-		ease: "power2.inOut",
-		gap: "2rem",
-		stagger: 0.3,
-		justifyContent: "start",
-		paddingRight: "5rem",
-		cursor: "grab",
-	});
-
-	// Reveal hidden content within the container
-	const revealHiddenContentAnimation = gsap.to(`#${container.id} .hidden`, {
-		duration: 1,
-		overflow: "visible",
-		width: "fit-content",
-		ease: "power2.inOut",
-		paddingLeft: "2rem",
-		stagger: 0.3,
-	});
-
-	// Wait for all animations to complete
-	await Promise.all([
-		snapping,
-		containerAnimation,
-		revealHiddenContentAnimation,
-	]);
-
-	// Reset GSAP scroll flag
-	isGsapScrolling = false;
-
-	// Set the current container
-	activeContainer = container;
-
-	// Retrieve sections within the container
-	const sections = openedContainers[container.id].sections;
-
-	// Initialize the current section
-	let currentSection = openedContainers[container.id].currentSection;
-
-	// Handle mouse movement within the container
-	container.addEventListener("mousemove", (e) => {
-		const mouseX = e.clientX;
-		if (handleMouseMovement(container, mouseX)) return;
-		container.style.cursor = "grab";
-	});
-
-	// Handle click events within the container
-	container.addEventListener("click", (e) => {
-		if (cursorPosition.right) {
-			currentSection =
-				currentSection === sections.length - 1
-					? currentSection
-					: currentSection + 1;
-		} else if (cursorPosition.left) {
-			currentSection = currentSection === 0 ? 0 : currentSection - 1;
-		}
-
-		// Scroll to the selected section
-		gsap.to(container, {
+		const snapping = gsap.to(window, {
 			duration: 1,
+			focus: true,
+			ease: "power2.inOut",
 			scrollTo: {
-				x: sections[currentSection].offsetLeft,
-				offsetX:
-					(window.innerWidth -
-						sections[currentSection % sections.length]
-							.offsetWidth) /
-					2,
+				y: container,
+				offsetY: verticalOffset,
 			},
-			cursor: cursorPosition.right
-				? "var(--cursor-right)"
-				: "var(--cursor-left)",
 		});
 
-		// Update the current section index
-		openedContainers[container.id].currentSection = currentSection;
-	});
+		// Animate the container's expansion
+		const containerAnimation = gsap.to(container, {
+			duration: 1,
+			x: "-5%",
+			width: "100vw",
+			overflowX: "hidden",
+			overflowY: "hidden",
+			ease: "power2.inOut",
+			stagger: 0.3,
+			justifyContent: "start",
+		});
+
+		const mainScroll = gsap.to(`#${container.id} .main-content`, {
+			overflowX: "scroll",
+			overflowY: "visible",
+			width: "100vw",
+		})
+
+		const revealAssets = gsap.to(
+			`#${container.id} .hero-image > img, #${container.id} .content > img, #${container.id} .content > gif, #${container.id} .content > video`,
+			{
+				duration: 1,
+				overflow: "visible",
+				width: "fit-content",
+				height: targetHeight,
+				ease: "power2.inOut",
+				stagger: 0.3,
+			}
+		);
+
+		const adjustingParagraphs = gsap.to(`#${container.id} .description`, {
+			duration: 1,
+			width: "40ch",
+			minWidth: "auto",
+			height: targetHeight,
+			ease: "power2.inOut",
+			overflowX: "hidden",
+			overflowY: "scroll",
+			stagger: 0.3,
+		});
+
+		// Reveal hidden content within the container
+		const revealHiddenContentAnimation = gsap.to(
+			`#${container.id} .hidden`,
+			{
+				duration: 1,
+				overflow: "visible",
+				width: "fit-content",
+				height: targetHeight,
+				ease: "power2.inOut",
+				paddingLeft: "2rem",
+				stagger: 0.3,
+			}
+		);
+
+		const fixingContentCover = gsap.to(`#${container.id} .content-cover`, {
+			ease: "none",
+			paddingLeft: "2rem",
+
+			// scrollTrigger: {
+			// 	trigger: `#${container.id} .main-content`,
+			// 	pin: true,
+			// 	scrub: true,
+			// 	// snap: 1 / (sections.length - 1),
+			// 	end: () =>
+			// 		"+=" + document.querySelector(`#${container.id} .main-content`).offsetWidth,
+			// },
+		});
+
+		Promise.all([
+			snapping,
+			containerAnimation,
+			revealHiddenContentAnimation,
+			revealAssets,
+			adjustingParagraphs,
+			fixingContentCover,
+		]);
+
+		activeContainer = container;
+	} else {
+		// openedContainers[container.id].sections.forEach((section) => {
+		// 	Draggable.create(section, {
+		// 		type: "left",
+		// 		bounds: ".container",
+		// 	});
+		// });
+
+		// Calculate target height and vertical offset for snapping
+		const viewHeight = window.innerHeight;
+		const targetHeight = viewHeight * 0.6;
+		const verticalOffset = (viewHeight - 590) / 2;
+
+		// Set flag for GSAP scroll animation
+		isGsapScrolling = true;
+
+		// Scale up the body
+		await gsap.to(document.body, {
+			duration: 1,
+			scale: 1,
+		});
+
+		// Snap to the container
+		const snapping = gsap.to(window, {
+			duration: 1,
+			focus: true,
+			ease: "power2.inOut",
+			scrollTo: {
+				y: container,
+				offsetY: verticalOffset,
+			},
+		});
+
+		// Animate the container's expansion
+		const containerAnimation = gsap.to(container, {
+			duration: 1,
+			x: 0,
+			width: "100%",
+			height: targetHeight,
+			overflowX: "scroll",
+			margin: "5rem 0",
+			ease: "power2.inOut",
+			gap: "2rem",
+			stagger: 0.3,
+			justifyContent: "start",
+			paddingRight: "5rem",
+			cursor: "grab",
+		});
+
+		// Reveal hidden content within the container
+		const revealHiddenContentAnimation = gsap.to(
+			`#${container.id} .hidden`,
+			{
+				duration: 1,
+				overflow: "visible",
+				width: "fit-content",
+				height: "100%",
+				ease: "power2.inOut",
+				paddingLeft: "2rem",
+				stagger: 0.3,
+			}
+		);
+
+		// Wait for all animations to complete
+		await Promise.all([
+			snapping,
+			containerAnimation,
+			revealHiddenContentAnimation,
+		]);
+
+		// Reset GSAP scroll flag
+		isGsapScrolling = false;
+
+		// Set the current container
+		activeContainer = container;
+
+		// Retrieve sections within the container
+		const sections = openedContainers[container.id].sections;
+
+		// Initialize the current section
+		let currentSection = openedContainers[container.id].currentSection;
+
+		// Handle mouse movement within the container
+		container.addEventListener("mousemove", (e) => {
+			const mouseX = e.clientX;
+			if (handleMouseMovement(container, mouseX)) return;
+			container.style.cursor = "grab";
+		});
+
+		// Handle click events within the container
+		container.addEventListener("click", (e) => {
+			if (cursorPosition.right) {
+				currentSection =
+					currentSection === sections.length - 1
+						? currentSection
+						: currentSection + 1;
+			} else if (cursorPosition.left) {
+				currentSection = currentSection === 0 ? 0 : currentSection - 1;
+			}
+
+			// Scroll to the selected section
+			gsap.to(container, {
+				duration: 1,
+				scrollTo: {
+					x: sections[currentSection].offsetLeft,
+					offsetX:
+						(window.innerWidth -
+							sections[currentSection % sections.length]
+								.offsetWidth) /
+						2,
+				},
+				cursor: cursorPosition.right
+					? "var(--cursor-right)"
+					: "var(--cursor-left)",
+			});
+
+			// Update the current section index
+			openedContainers[container.id].currentSection = currentSection;
+		});
+	}
 }
 
 // Event listener for scroll
 lenis.on("scroll", (e) => {
-	if (isGsapScrolling) return;
+	if (isGsapScrolling || isMobile) return;
 
 	gsap.to(activeContainer, {
 		width: "133.33%",
@@ -226,6 +330,8 @@ containers.forEach((container) => {
 						...container.childNodes[3].childNodes[3].childNodes,
 					].filter((node) => node.nodeName === "DIV"),
 				],
+				targetHeight: container.childNodes[3].offsetHeight,
+				bottomPadding: container.childNodes[1].offsetHeight,
 			};
 		}
 
@@ -244,10 +350,10 @@ videos.forEach((video) => {
 
 		// Check if mouse movement occurs within a container, handle it and return
 		if (handleMouseMovement(video, mouseX)) return;
-		else{
-			console.log('video mousemove');
+		else {
+			console.log("video mousemove");
 			e.stopPropagation();
-	
+
 			// Check if the video is playing and update cursor accordingly
 			video.style.cursor = isVideoPlaying(video)
 				? "var(--cursor-pause)"
@@ -291,6 +397,8 @@ carousels.forEach((carousel) => {
 		});
 	}, 3000);
 
+	if (isMobile) return;
+
 	// Event listener for mouse movements on the carousel
 	carousel.addEventListener("mousemove", (e) => {
 		const carouselWidth = carousel.offsetWidth;
@@ -299,10 +407,10 @@ carousels.forEach((carousel) => {
 
 		// Check if mouse movement occurs within the carousel, handle it and return
 		if (handleMouseMovement(carousel, mouseX)) return;
-		else{
-			console.log('carousel mousemove');
+		else {
+			console.log("carousel mousemove");
 			e.stopPropagation();
-	
+
 			// Update cursor based on mouse position
 			carousel.style.cursor =
 				mouseX > carouselLeft + carouselWidth * 0.5
